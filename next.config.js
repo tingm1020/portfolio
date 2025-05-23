9e3ee312547cd8a9a80f45c5cbf0fa4bdf19edd1
tingm1020 <58199984+tingm1020@users.noreply.github.com>
2025-05-23 15:22:37 +0800

    fix: update next.config.js and add export script

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'fakeimg.pl',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+    assetPrefix: './',
+  };
+  
+  export default nextConfig;
